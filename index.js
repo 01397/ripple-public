4db3b445ff217383a598574ec052c3a868cd3e74
01397 <41501869+01397@users.noreply.github.com>
2019-12-29 00:13:45 +0900

    gae test commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/test', (req, res) => {
+  result = {
+    timestamp: new Date().toUTCString(),
+    message: 'Hello everyone!!',
+  }
+  res.json(result)
+})
