8de9794dbf62ee029db6873b851b9ed25ae87d9d
01397 <41501869+01397@users.noreply.github.com>
2020-01-06 23:08:10 +0900

    M+ 1pフォント導入, Course選択画面作成

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,6 +6,36 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  public courses = [
+    { courseId: 'python-1st', title: 'Python 1st step', date: '7日前', progress: '0.8' },
+    { courseId: 'python-2nd', title: 'Python 2nd step', date: '9日前', progress: '0.6' },
+  ]
+  public selectedCourses = {
+    courseId: 'python-1st',
+    title: 'Python 1st step',
+    description:
+      'Pythonという言語を用いて、プログラミングの最初の一歩から学びます。ここに説明を書きます。ここに説明を書きます。ここに説明を書きます。ここに説明を書きます。',
+    progress: '0.8',
+    sections: [
+      {
+        title: '初めてのPython',
+        lessons: [
+          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
+          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
+          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
+        ],
+      },
+      {
+        title: '初めてのPython',
+        lessons: [
+          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
+          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
+          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
+        ],
+      },
+    ],
+  }
+
   constructor() {}
 
   ngOnInit() {}
