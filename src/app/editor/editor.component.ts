0ad406e9d8530d196ed147941c9df52173c1f7ed
01397 <41501869+01397@users.noreply.github.com>
2020-01-09 17:35:17 +0900

    websocket test

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core'
 import { AceEditorComponent } from 'ng2-ace-editor'
 import * as ace from 'ace-builds'
+import { WebsocketService } from '../websocket.service'
 
 @Component({
   selector: 'app-editor',
@@ -12,7 +13,14 @@ export class EditorComponent implements OnInit {
   text: string = ''
   options = { maxLines: 1000, printMargin: false }
 
+  constructor(private websocketService: WebsocketService) {}
+
   ngOnInit() {
     ace.config.set('basePath', 'path')
+    this.websocketService.connect()
+  }
+
+  onClick() {
+    this.websocketService.emit('test message', this.text)
   }
 }
---
+++ src/app/websocket.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core'
+import * as io from 'socket.io-client'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WebsocketService {
+  private socket
+
+  constructor() {}
+
+  connect() {
+    this.socket = io()
+    this.socket.on('connect', () => {
+      this.log('connected')
+    })
+    this.socket.on('connect_error', (err: object) => {
+      this.log('connect-error')
+      console.log(err)
+    })
+    this.socket.on('connect_timeout', () => {
+      this.log('connect_timeout')
+    })
+    this.socket.on('reconnect', (attempt: number) => {
+      this.log(`reconnect (attempt: ${attempt})`)
+    })
+    this.socket.on('reconnect_attempt', (attempt: number) => {
+      this.log(`reconnect_attempt (attempt: ${attempt})`)
+    })
+    this.socket.on('reconnecting', (attempt: number) => {
+      this.log(`reconnecting (attempt: ${attempt})`)
+    })
+    this.socket.on('reconnect_error', (err: object) => {
+      this.log('reconnect_error')
+      console.log(err)
+    })
+    this.socket.on('ping', () => {
+      this.log('ping')
+    })
+    this.socket.on('pong', (ms: number) => {
+      this.log(`pong (${ms}ms)`)
+    })
+    this.socket.on('test message', msg => {
+      this.log('test ' + msg)
+    })
+  }
+  emit(name: string, msg: string) {
+    this.log(name + ' ' + msg)
+    this.socket.emit(name, msg)
+  }
+
+  log(msg: string) {
+    console.log(`%cWebSocket%c ${msg}`, 'background-color: #ffaa00;color: #ffffff', 'color: #885500')
+  }
+}
