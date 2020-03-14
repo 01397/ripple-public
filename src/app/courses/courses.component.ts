ef00742678ba0f3197bd8824956e148d2467814c
朝尾直己 <41501869+01397@users.noreply.github.com>
2020-03-14 22:32:49 +0900

    レッスン選択 画面遷移

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core'
+import { AngularFirestore } from '@angular/fire/firestore'
+import { CourseItem, LessonItem, LessonItemId } from 'firestore-item'
+import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
+
+interface CourseItemId extends CourseItem {
+  id: string
+}
 
 @Component({
   selector: 'app-courses',
@@ -6,37 +14,57 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  public courses = [
-    { courseId: 'python-1st', title: 'Python 1st step', date: '7日前', progress: '0.8' },
-    { courseId: 'python-2nd', title: 'Python 2nd step', date: '9日前', progress: '0.6' },
-  ]
-  public selectedCourses = {
-    courseId: 'python-1st',
-    title: 'Python 1st step',
-    description:
-      'Pythonという言語を用いて、プログラミングの最初の一歩から学びます。ここに説明を書きます。ここに説明を書きます。ここに説明を書きます。ここに説明を書きます。',
-    progress: '0.8',
-    sections: [
-      {
-        title: '初めてのPython',
-        lessons: [
-          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
-          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
-          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
-        ],
-      },
-      {
-        title: '初めてのPython',
-        lessons: [
-          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
-          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
-          { title: 'Pythonとは', studyCount: 1, lastStudiedAt: '3日前', done: true },
-        ],
-      },
-    ],
+  private courseObservable: Observable<CourseItemId[]>
+  public courses: CourseItemId[]
+  public lessons: Observable<LessonItemId[]>
+  public selectedCourse: CourseItemId
+
+  constructor(public db: AngularFirestore) {}
+
+  ngOnInit() {
+    this.courseObservable = this.db
+      .collection<CourseItem>('course')
+      .snapshotChanges()
+      .pipe(
+        map(actions =>
+          actions.map(a => {
+            const data = a.payload.doc.data()
+            const id = a.payload.doc.id
+            return { id, ...data }
+          })
+        )
+      )
+    this.courseObservable.subscribe(courses => {
+      this.courses = courses
+    })
   }
 
-  constructor() {}
-
-  ngOnInit() {}
+  getCourseLastStudy(id: string) {
+    return ''
+  }
+  getCourseProgress(id: string) {
+    return ''
+  }
+  getLessonLastStudy(id: string) {
+    return ''
+  }
+  getLessonStudyCount(id: string) {
+    return ''
+  }
+  selectCourse(course: CourseItemId) {
+    this.selectedCourse = course
+    const courseId = course.id
+    this.lessons = this.db
+      .collection<LessonItemId>(`course/${courseId}/lesson`)
+      .snapshotChanges()
+      .pipe(
+        map(actions =>
+          actions.map(a => {
+            const data = a.payload.doc.data()
+            const id = a.payload.doc.id
+            return { id, courseId, ...data }
+          })
+        )
+      )
+  }
 }
