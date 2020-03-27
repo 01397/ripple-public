b1fae59886e8b9be9d0bbb5af48d62531965fa43
朝尾直己 <41501869+01397@users.noreply.github.com>
2020-03-27 18:11:20 +0900

    build時の問題修正

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyDaUDFu8GHzREw057J7ehzJOheQ1y5Jp4s',
+    authDomain: 'ripple-public.firebaseapp.com',
+    databaseURL: 'https://ripple-public.firebaseio.com',
+    projectId: 'ripple-public',
+    storageBucket: 'ripple-public.appspot.com',
+    messagingSenderId: '1010671820817',
+    appId: '1:1010671820817:web:8edda9ebeaa6e5f401d1b0',
+    measurementId: 'G-26TXFB16FH',
+  },
 }
