74d265a59853cbb9c712cd99e23455834cb5b9d5
朝尾直己 <41501869+01397@users.noreply.github.com>
2020-04-24 09:28:08 +0900

    not foundページのルーティング等

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use('/*', express.static(path.join(__dirname, '/dist/angular-app/index.html'
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
+  res.redirect(404, '/404')
 })
 
 // error handler
